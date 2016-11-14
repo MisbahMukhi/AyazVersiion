62faf036fc67e0f3c0fae8bbb5d86fbb2f6b410e
TranK <TranK@DESKTOP-IGBHPFS>
2016-11-14 11:55:57 -0600

    InitialSetup

---
+++ LonghornMusic/LonghornMusic/Models/SalesByGenre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LonghornMusic.Models
+{
+    public class SalesByGenre
+    {
+        public int Count { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ LonghornMusic/LonghornMusic/Views/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using LonghornMusic.Models;
+
+namespace LonghornMusic.ViewModels {
+    public class ShoppingCartViewModel {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
