1e065f3bd1a161fe0a3a524cf0b8244e2022e1f4
TranK <TranK@DESKTOP-IGBHPFS>
2016-11-15 17:48:08 -0600

    11/15/2016 5:48 PM

--- LonghornMusic/LonghornMusic/Views/ViewModels/ShoppingCartViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using LonghornMusic.Models;
-
-namespace LonghornMusic.ViewModels {
-    public class ShoppingCartViewModel {
-        public List<Cart> CartItems { get; set; }
-        public decimal CartTotal { get; set; }
-    }
-}
